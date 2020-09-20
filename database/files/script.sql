31862a396871e67e078ea5b0f8e03fc56dfd695e
Gabriel <gabrielsantoscimol@gmail.com>
2020-09-19 22:08:40 -0300

    Add database user table (#8)

--- database/files/script.sql
+++ database/files/script.sql
@@ -1,5 +1,18 @@
-CREATE TABLE "TodoItem" (
-  "Id" SERIAL, 
-  "Description" VARCHAR,
-  "Done" BOOLEAN
+CREATE TABLE "User" (
+  "Id" SERIAL PRIMARY KEY,
+  "Name" VARCHAR(100) NOT NULL,
+  "Email" VARCHAR(100) NOT NULL,
+  "Password" VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE "TodoItem" (
+  "Id" SERIAL PRIMARY KEY,
+  "Description" VARCHAR(300) NOT NULL,
+  "Done" BOOLEAN,
+  "UserId" INT NOT NULL REFERENCES "User"("Id")
+);
+
+INSERT INTO "User" ("Name", "Email", "Password") 
+  VALUES
+('admin', 'admin@mail.com', 'admin123'),
+('contact', 'contact@mail.com', 'contact123');
